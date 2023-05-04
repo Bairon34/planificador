07f4ede3e0f491a30b2d29d9c6c8b611c9fbbc5e
Bairon Imbacuan <bairon.imbacuan@valid.com>
2023-05-04 14:34:19 -0500

    initial commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+     
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
